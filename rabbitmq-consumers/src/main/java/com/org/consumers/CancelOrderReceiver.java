1ef229527ad05256614b6a0ea344bad39fea6067
kkj <2798605887@qq..com>
2020-07-01 00:09:16 +0800

    订单延迟队列

---
+++ rabbitmq-consumers/src/main/java/com/org/consumers/CancelOrderReceiver.java
@@ -0,0 +1,21 @@
+package com.org.consumers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RabbitListener(queues = "hello_queue")
+public class CancelOrderReceiver {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(CancelOrderReceiver.class);
+
+    @RabbitHandler
+    public void handle(Long orderId){
+        LOGGER.info("消息被消费成功");
+        LOGGER.info("process orderId:{}",orderId);
+    }
+}
