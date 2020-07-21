c0bab06911b353bbf611d534dbe917d88e8cf305
kkj <2798605887@qq..com>
2020-07-21 12:45:19 +0800

    发布

--- rabbitmq-consumers/src/main/java/com/org/consumers/RabbitmqConsumersApplication.java
+++ rabbitmq-consumers/src/main/java/com/org/consumers/RabbitmqConsumersApplication.java
@@ -9,7 +9,7 @@ public class RabbitmqConsumersApplication {
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqConsumersApplication.class, args);
         System.out.println("==========我是消费者启动=======");
-        System.out.printf("天青色等烟2336673");
+        System.out.printf("天青色等烟雨");
     }
 
 
